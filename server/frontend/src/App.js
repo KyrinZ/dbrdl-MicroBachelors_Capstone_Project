d281b194a3ac188c8501f3e0617cce7f5edff077
Bayajit Mohammed <bayajitmohammed@gmail.com>
2022-10-01 23:36:54 +0600

    setup frontend and backend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,26 +1,43 @@
-import './bootstrap.min.css';
-import React from 'react';
-import HomePage from './homepage.js';
-import ContactUs from './contactus.js';
+import "./bootstrap.min.css";
+import React from "react";
+import HomePage from "./homepage.js";
+import ContactUs from "./contactus.js";
+import AboutUs from "./aboutpage.js";
 
-/*
-Copy the Web Chat code from Watson Assitant and paste it here
-*/
+window.watsonAssistantChatOptions = {
+  integrationID: "2c50c769-91b8-4e33-a4c9-cd6a0fc9c8be", // The ID of this integration.
+  region: "jp-tok", // The region your integration is hosted in.
+  serviceInstanceID: "db74e5ee-a888-4691-a3f8-ef7d70be2ea6", // The ID of your service instance.
+  onLoad: function (instance) {
+    instance.render();
+  },
+};
+setTimeout(function () {
+  const t = document.createElement("script");
+  t.src =
+    "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
+    (window.watsonAssistantChatOptions.clientVersion || "latest") +
+    "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
 
 class App extends React.Component {
-
   state = {
-    pageshown:<HomePage/>
-  }
+    pageshown: <HomePage />,
+  };
 
-  setPageHome = ()=> {
-    this.setState({pageshown:<HomePage/>});    
-  }
-  
-  setPageContactUs = ()=> {
-    this.setState({pageshown:<ContactUs/>});    
-  }
-  
+  setPageHome = () => {
+    this.setState({ pageshown: <HomePage /> });
+  };
+
+  setPageContactUs = () => {
+    this.setState({ pageshown: <ContactUs /> });
+  };
+
+  setPageAboutUs = () => {
+    this.setState({ pageshown: <AboutUs /> });
+  };
+
   render() {
     return (
       <div>
@@ -28,21 +45,25 @@ class App extends React.Component {
           <div class="navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
               <li class="nav-item active">
-                <a class="nav-link" href="#" onClick={this.setPageHome}>Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#" onClick={this.setPageHome}>
+                  Home <span class="sr-only">(current)</span>
+                </a>
               </li>
-                /*
-                Add more tabs and action items for the tabs to give the dealership website a real feel. 
-                */
-        
               <li class="nav-item">
-                <a class="nav-link" href="#" onClick={this.setPageContactUs}>Contact Us</a>
+                <a class="nav-link" href="#" onClick={this.setPageAboutUs}>
+                  About Us
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#" onClick={this.setPageContactUs}>
+                  Contact Us
+                </a>
               </li>
             </ul>
           </div>
         </nav>
-        <div>
-          {this.state.pageshown}
-        </div>
+        <div>{this.state.pageshown}</div>
       </div>
     );
   }
---
+++ server/frontend/src/aboutpage.js
@@ -0,0 +1,30 @@
+import "./bootstrap.min.css";
+import React from "react";
+
+class AboutUs extends React.Component {
+  render() {
+    return (
+      <div>
+        <div style={{ display: "flex" }}>
+          <img
+            src="https://cdn.pixabay.com/photo/2016/03/11/08/02/usa-1249880__340.jpg"
+            style={{ width: "300px" }}
+          />
+          <div style={{ margin: "100px" }}>
+            <span style={{ margin: "60px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AboutUs;
